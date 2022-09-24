c58aa5a458d9877db85382890ed52e1d1588dac6
taoyes003 <taoyes003@gmail.com>
2022-09-24 18:48:10 +0800

    project init

---
+++ src/test/java/com/taoyes3/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoyes3.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
